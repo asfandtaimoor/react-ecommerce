51de187a48c8aa0d0ed09e3a8d8706b686fdf75e
Taimoor Salyhal <asfandtaimoor786@gmail.com>
2021-01-24 09:52:01 +0500

    Banner text-section is added

--- src/Components/Banner/Banner.component.jsx
+++ src/Components/Banner/Banner.component.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./Banner.style.css"; // StyleSheet
 const Banner = () => {
-  return <div className="banner-container"></div>;
+  return (
+    <div className="banner-container">
+      <div className="banner-text">
+        <h1>Eid ke run apno ke sung</h1>
+        <p>Enjoy online shopping with pakExpress</p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
